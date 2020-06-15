dc39a95380eb3a65bceaaf0f0c69e9799ea22c55
Yonglin Li <yonglin.li@gmail.com>
2020-06-14 22:21:37 -0400

    Yonglin submitted

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,11 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="switchComponent(`app-blue`)">Load Blue Template</button>
+                <button class="btn btn-success" @click="switchComponent(`app-green`)">Load Green Template</button>
+                <button class="btn btn-danger"  @click="switchComponent(`app-red`)">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="selectedComponent"><p>I say hello!</p></component>                
             </div>
         </div>
     </div>
@@ -21,10 +19,20 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function(){
+            return {
+                selectedComponent: `app-blue`
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
             appRed: Red
+        },
+        methods: {
+            switchComponent: function(c){
+                this.selectedComponent = c;
+            }
         }
     }
 </script>
