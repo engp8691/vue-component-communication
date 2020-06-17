05c4904db036b5b6007de94b2ab27876db4642ff
Yonglin Li <yonglin.li@gmail.com>
2020-06-16 23:31:59 -0400

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div class="container">
-    <app-header></app-header>
-    <hr />
     <div class="row">
-      <servers></servers>
-      <app-server-details></app-server-details>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-text="'Some String Text here!'"></p>
+        <p v-html="'<strong>Some HTML Text'"></p>
+      </div>
     </div>
     <hr />
-    <app-footer></app-footer>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Customized Directives</h1>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Hello World</p>
+        <p v-highlight="'blue'">Hello World Text</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "./components/Shared/Header.vue";
-import Footer from "./components/Shared/Footer.vue";
-import Servers from "./components/Server/Servers.vue";
-import ServerDetails from "./components/Server/ServerDetails.vue";
-
 export default {
-  components: {
-    appHeader: Header,
-    Servers,
-    "app-server-details": ServerDetails,
-    "app-footer": Footer
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        console.log(binding);
+        let delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          if (binding.arg == "background") {
+            setTimeout(() => {
+              el.style.backgroundColor = binding.value;
+            }, delay);
+          } else {
+            el.style.color = binding.value;
+          }
+        }
+      }
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const serverBus = new Vue();
+Vue.directive('highlight', {
+  bind(el, binding, vnode){
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    console.log(binding);
+
+    let delay = 0;
+    if(binding.modifiers['delayed']){
+      delay = 3000;
+    }
+    if(binding.arg == 'background'){
+      setTimeout(()=>{
+        el.style.backgroundColor = binding.value;
+      }, delay);      
+    }else{
+      el.style.color = binding.value;
+    }
+  }
+});
 
 new Vue({
   el: '#app',
